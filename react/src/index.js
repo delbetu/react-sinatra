f3e908b2972002ec38a641a238bae5ea465ef1a4
M.Bellucci <delbetu@gmail.com>
2020-01-24 18:41:01 -0300

    Add rootReducer with dummy action.

--- react/src/index.js
+++ react/src/index.js
@@ -1,5 +1,38 @@
 import React, { Component } from 'react'
 import ReactDom from 'react-dom'
+import Provider from 'react-redux'
 import App from './components/app'
+import { createStore } from 'redux'
 
-ReactDom.render(<App place='index-page' />, document.getElementById('main-content'))
+const ADD_NUMBER = 'ADD_NUMBER'
+const add_number_action = (number) => ({
+  type: ADD_NUMBER,
+  payload: number
+})
+
+const initialState = {
+  numbers: []
+}
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_NUMBER:
+      return Object.assign({}, state, { numbers: state.numbers.concat(action.payload) });
+    default:
+      return state
+  }
+}
+
+const store = createStore(rootReducer, initialState)
+
+store.dispatch(add_number_action(1))
+store.dispatch(add_number_action(2))
+store.dispatch(add_number_action(3))
+store.dispatch(add_number_action(4))
+
+console.log(store.getState())
+
+ReactDom.render(
+  <App place='index-page' />
+  ,
+  document.getElementById('main-content')
+)

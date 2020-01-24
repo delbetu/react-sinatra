d871605b05ac16d2fd2cb8f6d1db520df301bc4a
M.Bellucci <delbetu@gmail.com>
2020-01-24 11:42:55 -0300

    remove all source code

--- react/src/collections/user-collection.js
+++
@@ -1,8 +0,0 @@
-import User from '../models/user-model.js'
-
-let UserCollection = Backbone.Collection.extend({
-  model: User,
-  url: process.env.apiHost+'/api/v1/users',
-});
-
-export default UserCollection

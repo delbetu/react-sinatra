3ede8baf44ae22492cfa33eaabf1584378e7fc9b
M.Bellucci <delbetu@gmail.com>
2020-01-25 09:23:15 -0300

    Suggest a random int for next addition.

--- react/src/components/add-number-form.js
+++ react/src/components/add-number-form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ADD_NUMBER  } from "../constants/action-types";
+import { ADD_NUMBER  } from "../constants/action-types"
 
 const add_number_action = (number) => ({
   type: ADD_NUMBER,
@@ -9,25 +9,26 @@ const add_number_action = (number) => ({
 function mapDispatchToProps(dispatch) {
   return {
     addNumber: n => dispatch(add_number_action(n))
-  };
+  }
 }
 
 import { connect  } from "react-redux"
 class ConnectedForm extends Component {
   constructor(props) {
-    super(props);
-    this.state = { number: "1" };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    super(props)
+    this.state = { number: "1" }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
   handleChange(event) {
-    this.setState({ [event.target.id]: event.target.value  });
+    this.setState({ [event.target.id]: event.target.value  })
   }
   handleSubmit(event) {
-    event.preventDefault();
-    const { number } = this.state;  // extract number from state
-    this.props.addNumber(number);
-    this.setState({ number: ""  });
+    event.preventDefault()
+    const { number } = this.state
+    this.props.addNumber(number)
+    const randInt = Math.ceil(Math.random() * 10) // Suggest a new number
+    this.setState({ number: randInt  })
   }
 
   render() {
@@ -36,7 +37,7 @@ class ConnectedForm extends Component {
         <input type="text" id="number" value={this.state.number} onChange={this.handleChange} />
         <button type="submit">ADD</button>
       </form>
-    );
+    )
   }
 }
 

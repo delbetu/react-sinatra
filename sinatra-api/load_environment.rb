ab777c9a00a0107ac51d5598e9bff9a51e3422a9
M.Bellucci <delbetu@gmail.com>
2020-02-28 18:09:06 -0300

    Load database credentials from config file.

--- sinatra-api/load_environment.rb
+++ sinatra-api/load_environment.rb
@@ -7,9 +7,20 @@ Bundler.require(:default, ENV['RACK_ENV'].to_sym)
 # Load all files under lib
 Dir["#{APP_ROOT}/lib/**/*.rb"].each {|file| require file }
 
+
+require 'yaml'
+
+require 'pry'
+
+db_config = YAML::load_file("#{APP_ROOT}/config/database.yml.erb")[ENV.fetch('RACK_ENV', 'development')]
+db_username = ERB.new(db_config['username']).result
+db_password = ERB.new(db_config['password']).result
+db_host = ERB.new(db_config['host']).result
+db_name = ERB.new(db_config['database']).result
+
 MAIN_CONTAINER = ROM.container(
   :sql,
-  'postgres://app_user_development:@localhost/app_db_development',
+  "postgres://#{db_username}:#{db_password}@#{db_host}/#{db_name}",
   extensions: [:pg_json]
 ) do |config|
   config.auto_registration("#{APP_ROOT}/lib/")
